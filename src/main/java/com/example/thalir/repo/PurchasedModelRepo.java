59149347d238bbf97a92121fabe3f814c603b4e0
yoshwa <ryoshwaa@gmail.com>
2025-04-08 01:18:58 +0530

    Added local files

---
+++ src/main/java/com/example/thalir/repo/PurchasedModelRepo.java
@@ -0,0 +1,7 @@
+package com.example.thalir.repo;
+
+import com.example.thalir.model.PurchasedModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PurchasedModelRepo extends JpaRepository<PurchasedModel, Long>{
+}
---
+++ src/main/java/com/example/thalir/repo/UserRepo.java
@@ -0,0 +1,7 @@
+package com.example.thalir.repo;
+
+import com.example.thalir.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+}
